b89f76a0bd2635cb32a0d8b180a9bb0ba9782e06
Vredinburgh <ianvredenburg@hotmail.com>
2014-12-02 12:49:42 +0100

    commit

---
+++ game.js
@@ -0,0 +1,15 @@
+//namespace
+goog.provide('zoo_hunter.Game');
+
+
+//dependencies
+goog.require('lime.Director');
+goog.require('lime.GlossyButton');
+goog.require('lime.Layer');
+goog.require('lime.Scene');
+goog.require('lime.audio.Audio');
+
+zoo_hunter.Game = function(mode) {
+
+
+}
