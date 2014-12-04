4c250031b02b97a08b6a2b4810d0d4b11e6d8c8a
Vredinburgh <ianvredenburg@hotmail.com>
2014-12-04 15:25:46 +0100

    Can now dynamically load sprites

--- game.js
+++ game.js
@@ -4,34 +4,36 @@ goog.require('lime.Director');
 goog.require('lime.GlossyButton');
 goog.require('lime.Layer');
 goog.require('lime.Scene');
-goog.require('lime.audio.Audio');
 
 zoo.Game = function(mode) {
 	lime.Sprite.call(this);
 
 	var mode = mode,
 		WIDTH = 1280,
-		HEIGHT = 720;
+		HEIGHT = 720,
+		posX = 0,
+		posY = 0,
+		anchX = 0,
+		anchY = 0;
 
 	switch (mode) {
 		case 1:
-			var posX = 0,
-				posY = 0,
-				anchX = 0,
-				anchY = 0;
 			break;
 		case 2:
-			this.posX = 0;
-			this.posY = 0;
-			this.anchX = 0;
-			this.anchY = 0;
+			posX = 0; 
+			posY = 0; 
+			anchX = 0; 
+			anchY = 0;
 			break;	
 	}
 
-	var layer = new lime.Layer().setPosition(posX,posY).setRenderer(lime.Renderer.CANVAS).setAnchorPoint(anchX, anchY),
-    	map = new lime.Sprite().setSize(WIDTH,HEIGHT).setFill('assets/maps/map.png').setPosition(posX, posY).setAnchorPoint(anchX,anchY);
+	var newSprites = Array();
+	newSprites[0] = new lime.Sprite().setSize(WIDTH,HEIGHT).setFill('assets/maps/map.png').setPosition(posX, posY).setAnchorPoint(anchX,anchY);
+	newSprites[1] = new lime.Sprite().setSize(60,100).setFill('assets/sprites/illuminati.png').setPosition(200,200);
+	
 
-    return map;
+
+    return newSprites;
     
 
 }
--- zoo.js
+++ zoo.js
@@ -7,25 +7,36 @@ goog.require('lime.Director');
 goog.require('lime.GlossyButton');
 goog.require('lime.Layer');
 goog.require('lime.Scene');
-goog.require('zoo.Game')
+goog.require('lime.audio.Audio');
+goog.require('zoo.Game');
 
 //entry point
 zoo.start = function() {
 	zoo.director = new lime.Director(document.body,1280,720);
 	var scene = new lime.Scene(),
 		layer = new lime.Layer();
+		
 
-	var btn = new lime.GlossyButton('PLAY').setSize(200, 80).setPosition(640, 360);
+	var btn = new lime.GlossyButton('PLAY').setSize(200, 80).setPosition(640, 260);
 		goog.events.listen(btn, 'click', function() {
 			zoo.newgame(1);
+	});
+	layer.appendChild(btn);
+
+	var btn = new lime.GlossyButton('LOAD').setSize(200, 80).setPosition(640, 460);
+		goog.events.listen(btn, 'click', function() {
+			zoo.newgame(2);
 	});
 	layer.appendChild(btn);
 
 	scene.appendChild(layer);
 
 	zoo.director.makeMobileWebAppCapable();
-
+	zoo.director.setDisplayFPS(false);
 	zoo.director.replaceScene(scene);
+
+	//var sound = new lime.audio.Audio('assets/audio/x-files.mp3');
+	//zoo.sound.play();
 }
 
 zoo.newgame = function(mode) {
@@ -35,8 +46,25 @@ zoo.newgame = function(mode) {
 	scene.appendChild(layer);
 
 	var game = new zoo.Game(mode);
-	layer.appendChild(game);
+	var map = game[0];
+	var sprite = game[1];
 	
+	
+	layer.appendChild(map);
+	layer.appendChild(sprite);
 
 	zoo.director.replaceScene(scene);
+
+	//initiate controls
+	//this.move = new zoo.Sprite(map, sprite);
+
+
+	//var sprite = new zoo.Sprite();
+	//layer.appendChild(sprite);
+
+	//var newMap = zoo.Game.map;
+	//layer.appendChild(newMap);
+	
+
+	
 };
